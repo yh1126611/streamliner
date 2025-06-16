d2af54c4c470b884085e4f9deca0449887de1861
Young Ho Lee <64393740+yh1126611@users.noreply.github.com>
2025-06-16 12:12:35 +0900

    Update streamliner.sh

--- conda/streamliner.sh
+++ conda/streamliner.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 if [[ "$1" == "--version" ]]; then
-    echo "Version 1.1.0"
+    echo "Version 1.2.0"
     exit 0
 fi
 
 if [[ "$1" == "--help" ]]; then
     echo ""
     echo "Program:        streamliner"
-    echo "Version:        1.1.0"
+    echo "Version:        1.2.0"
     echo "Code:           https://github.com/yh1126611/streamliner"
     echo ""
     echo "Description:"
@@ -17,9 +17,9 @@ if [[ "$1" == "--help" ]]; then
     echo "Resulting output may be used as data for visualization in R."
     echo ""
     echo "Usage:"
-    echo "streamliner [OPTIONS] <species_information.txt|tsv>"
+    echo "streamliner [OPTIONS] <BURST.txt|tsv>"
     echo ""
-    echo "<species_information>   A two-column tab-delimited file with following"
+    echo "<BURST>                 A two-column tab-delimited file with following"
     echo "                        columns:"
     echo "                        1. NCBI RefSeq accession ID"
     echo "                        2. Links to PacBio HiFi sequencing reads on"
@@ -43,9 +43,10 @@ if [[ "$1" == "--help" ]]; then
     echo "                        analyzed (bp) [Default 10,000]"
     echo "-w, --window    INT     Size of each window inside interval (bp)."
     echo "                        [Default 100]"
-    echo "-p, --partial   INT     Value between 1 and 10 indicating which stage"
-    echo "                        to start process if pipeline is to be run"
-    echo "                        partially [Default 0 (Complete pipeline)]"
+    echo "-p, --partial   INT     Two values between 0 and 10 indicating which"
+    echo "                        stages to start and end streamliner,"
+    echo "                        respectively if pipeline is to be run"
+	echo "                        partially [Default 0, 10 (Complete pipeline)]"
     echo ""
     exit 0
 fi
@@ -55,7 +56,8 @@ maxmem="99G"
 type_string="gene"
 window_size=100
 interval_size=10000
-partial=0
+partial_start=0
+partial_end=10
 
 # Manual option parsing to handle both short and long options
 while [[ $# -gt 0 ]]; do
@@ -76,14 +78,11 @@ while [[ $# -gt 0 ]]; do
             interval_size="$2"
             shift 2
             ;;
-        -p|--partial)
-            partial="$2"
-            if ! [[ "$partial" =~ ^[1-9]$|^1[0-9]$|^2[0-7]$ ]]; then
-                echo "Error: -p|--partial must be an integer between 1 and 27" >&2
-                exit 1
-            fi
-            shift 2
-            ;;
+		-p|--partial)
+    		partial_start="$2"
+    		partial_end="$3"
+			shift 3
+			;;
         -*)
             echo "Invalid option: $1" >&2
             exit 1
@@ -121,22 +120,27 @@ for var_name in interval_size window_size; do
 done
 
 if [[ -n "$maxmem" && ! "$maxmem" =~ ^-?[0-9]+[KMG]?$ ]]; then
-  echo "Error: Option -m must be an integer or an integer followed by K, M, or G (e.g. 100M)." >&2
-  exit 1
+	echo "Error: Option -m must be an integer or an integer followed by K, M, or G (e.g. 100M)." >&2
+	exit 1
 fi
 
 # Check if interval is bigger than window
 if [ "$interval_size" -le "$window_size" ]; then
     echo "Error: Interval size ($interval_size) must be larger than window size ($window_size)" >&2
-    exit 1
+	exit 1
 fi
 
 if [[ -n "$type_string" && ! "$type_string" =~ ^(gene|transcript|mRNA)$ ]]; then
-  echo "Error: Option -t must be one of 'gene', 'transcript', or 'mRNA'." >&2
-  exit 1
+	echo "Error: Option -t must be one of 'gene', 'transcript', or 'mRNA'." >&2
+	exit 1
 fi
 
-if [ "$partial" -le 0 ]; then
+if ! [[ "$partial_start" =~ ^[0-9]+$ && "$partial_end" =~ ^[0-9]+$ ]]; then
+	echo "Error: -p|--partial requires two integer arguments between 0 and 10." >&2
+	exit 1
+fi
+
+if [ "$partial_start" -le 0 ] && [ "$partial_end" -ge 0 ]; then
 	# Extract accession numbers
 	awk -v FS=" " -v OFS="\t" '$1~/^GCF/{print $1}' "$read_links" | while read accession; do
 		echo $accession >> accession.txt
@@ -179,42 +183,42 @@ fi
 # Pipeline (modification score calling -> TSS/TES profile identification -> GC/BpB content calculation)
 awk -v FS="\t|/" -v OFS="\t" '$1~/^GCF/{print $10}' "$read_links" | while read -r col1; do
 	cd "$col1"
-	if [ "$partial" -le 1 ]; then
+	if [ "$partial_start" -le 1 ] && [ "$partial_end" -ge 1 ]; then
 		samtools merge "$col1".bam *.bam
 	fi
-	if [ "$partial" -le 2 ]; then
+	if [ "$partial_start" -le 2 ] && [ "$partial_end" -ge 2 ]; then
 		[ -f "$col1".bam ] || { echo "Error: Merged BAM file for ${col1} not found"; exit 1; }
 		extracthifi "$col1".bam "$col1"_hifi.bam
 	fi
-	if [ "$partial" -le 3 ]; then
+	if [ "$partial_start" -le 3 ] && [ "$partial_end" -ge 3 ]; then
 		[ -f "$col1"_hifi.bam ] || { echo "Error: HiFi-extracted BAM file for ${col1} not found"; exit 1; }
 		jasmine "$col1"_hifi.bam "$col1"_hifi_methCalled.bam
 	fi
-	if [ "$partial" -le 4 ]; then
+	if [ "$partial_start" -le 4 ] && [ "$partial_end" -ge 4 ]; then
 		[ -f "$col1"_hifi_methCalled.bam ] || { echo "Error: Modification-called BAM file for ${col1} not found"; exit 1; }
 		pbmm2 align *.fasta "$col1"_hifi_methCalled.bam > "$col1"_hifi_methCalled_aligned.bam
 	fi
-	if [ "$partial" -le 5 ]; then
+	if [ "$partial_start" -le 5 ] && [ "$partial_end" -ge 5 ]; then
 		[ -f "$col1"_hifi_methCalled_aligned.bam ] || { echo "Error: Aligned BAM file for ${col1} not found"; exit 1; }
 		samtools sort -m "$maxmem" "$col1"_hifi_methCalled_aligned.bam -o "$col1"_hifi_methCalled_aligned_sorted.bam
 	fi
-	if [ "$partial" -le 6 ]; then
+	if [ "$partial_start" -le 6 ] && [ "$partial_end" -ge 6 ]; then
 		[ -f "$col1"_hifi_methCalled_aligned_sorted.bam ] || { echo "Error: Sorted BAM file for ${col1} not found"; exit 1; }
 		samtools index "$col1"_hifi_methCalled_aligned_sorted.bam "$col1"_hifi_methCalled_aligned_sorted.bam.bai
 	fi
-	if [ "$partial" -le 7 ]; then
+	if [ "$partial_start" -le 7 ] && [ "$partial_end" -ge 7 ]; then
 		[ -f "$col1"_hifi_methCalled_aligned_sorted.bam ] || { echo "Error: Sorted BAM file for ${col1} not found"; exit 1; }
 		aligned_bam_to_cpg_scores --bam "$col1"_hifi_methCalled_aligned_sorted.bam --output-prefix "$col1" --modsites-mode reference --ref *.fasta
 		gzip -df *.bed.gz
 		awk -v FS="\t" -v OFS="\t" '$1!~"^#"{print $1, $2, $3, $4}' *combined.bed > CG.mp
 	fi
-	if [ "$partial" -le 8 ]; then
+	if [ "$partial_start" -le 8 ] && [ "$partial_end" -ge 8 ]; then
 		bedtools intersect -wo -a CG.mp -b <(awk -v type_string="$type_string" -v FS="\t" -v OFS="\t" '{if($7~/^\+$/&&$3==type_string){print $1, $4, $7} else if($7~/^\-$/&&$3==type_string){print $1, $5, $7}}' *.gtf | sort -n | uniq | awk -v FS="\t" -v OFS="\t" '{print $1, $2-10000, $2+10000, $3}' | awk -v FS="\t" -v OFS="\t" '{if($2<0){print $1, 0, $3, $2+10000, $4}else{print $1, $2, $3, $2+10000, $4}}') | awk -v FS="\t" -v OFS="" '{print $5, ".", $8, "\t", $2-$8, "\t", $4, "\t", $9}' > MP_TSS_"$col1".tsv
 		bedtools intersect -wo -a CG.mp -b <(awk -v type_string="$type_string" -v FS="\t" -v OFS="\t" '{if($7~/^\+$/&&$3==type_string){print $1, $5, $7} else if($7~/^\-$/&&$3==type_string){print $1, $4, $7}}' *.gtf | sort -n | uniq | awk -v FS="\t" -v OFS="\t" '{print $1, $2-10000, $2+10000, $3}' | awk -v FS="\t" -v OFS="\t" '{if($2<0){print $1, 0, $3, $2+10000, $4}else{print $1, $2, $3, $2+10000, $4}}') | awk -v FS="\t" -v OFS="" '{print $5, ".", $8, "\t", $2-$8, "\t", $4, "\t", $9}' > MP_TES_"$col1".tsv
 		sed -i '/^NC/!d' MP_TSS_"$col1".tsv
 		sed -i '/^NC/!d' MP_TES_"$col1".tsv
 	fi
-	if [ "$partial" -le 9 ]; then
+	if [ "$partial_start" -le 9 ] && [ "$partial_end" -ge 9 ]; then
 		awk -v type_string="$type_string" -v FS="\t" -v OFS="\t" '{if($7~/^\+$/&&$3==type_string){print $1, $4, $7} else if($7~/^\-$/&&$3==type_string){print $1, $5, $7}}' *.gtf | sort | uniq > TSS.txt
 		awk -v type_string="$type_string" -v FS="\t" -v OFS="\t" '{if($7~/^\+$/&&$3==type_string){print $1, $5, $7} else if($7~/^\-$/&&$3==type_string){print $1, $4, $7}}' *.gtf | sort | uniq > TES.txt
 		sed -i '/^NC/!d' TSS.txt
@@ -222,7 +226,7 @@ awk -v FS="\t|/" -v OFS="\t" '$1~/^GCF/{print $10}' "$read_links" | while read -
 		streamgc *.fasta TSS.txt GC_TSS_"$col1".tsv
 		streamgc *.fasta TES.txt GC_TES_"$col1".tsv
 	fi
-	if [ "$partial" -le 10 ]; then
+	if [ "$partial_start" -le 10 ] && [ "$partial_end" -ge 10 ]; then
 		streambpb -b CC -w "$window_size" -i "$interval_size" *.fasta TSS.txt CpC_TSS_"$col1".tsv
 		streambpb -b CC -w "$window_size" -i "$interval_size" *.fasta TES.txt CpC_TES_"$col1".tsv
 		streambpb -b CG -w "$window_size" -i "$interval_size" *.fasta TSS.txt CpG_TSS_"$col1".tsv
